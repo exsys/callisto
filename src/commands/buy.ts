471d95af1ba1fac00984a3e117d4d23c87920802
exsyss <exsysb@gmail.com>
2024-07-13 10:03:28 +0200

    added /buy command

--- src/commands/buy.ts
+++ src/commands/buy.ts
@@ -1,14 +1,26 @@
 import { SlashCommandBuilder } from "discord.js";
-import { Wallet } from "../models/wallet";
+import { createBuyModal, createPreBuyUI } from "../lib/discord-ui";
+import { SolanaWeb3 } from "../lib/solanaweb3";
+import { UI } from "../interfaces/ui";
 
 const command = {
     data: new SlashCommandBuilder()
         .setName("buy")
-        .setDescription("Buy a given coin. If no options are given, default exchange and coin are used."),
+        .setDescription("Buy a voin with the given contract address.")
+        .addStringOption(option => option.setName('Contract Address')
+            .setDescription('The contract address of the Solana Token')),
     async execute(interaction: any) {
         try {
             await interaction.deferReply({ ephemeral: true });
-            await interaction.editReply("not implemented yet");
+            const contractAddress = interaction.options.getString("Contract Address");
+            const isValidAddress = SolanaWeb3.checkIfValidAddress(contractAddress);
+            if (!isValidAddress) {
+                await interaction.editReply({ content: "Invalid contract address. Please enter a valid contract address.", ephemeral: true });
+                return;
+            }
+
+            const buyUI: UI = await createPreBuyUI(interaction.user.id, contractAddress);
+            await interaction.editReply(buyUI);
         } catch (error) {
             await interaction.editReply("Server error. Please try again later.");
         }
